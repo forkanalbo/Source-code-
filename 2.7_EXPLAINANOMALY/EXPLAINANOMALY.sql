14491ca5ec04733c115700bfbcafcb05e3a4ada5
forkan albo <forkan.albo@mail.ru>
2021-01-29 17:30:30 +0500

    update9

--- 2.7_EXPLAINANOMALY/EXPLAINANOMALY.sql
+++ 2.7_EXPLAINANOMALY/EXPLAINANOMALY.sql
@@ -103,4 +103,180 @@ END;
 $$
 LANGUAGE plpgsql;
 
+/* Создание таблиц правил
+tableName - имя таблицы
+sensorNum - количество сенсоров
+ruleNum - количество правил
+Результат выполения:
+Создана таблица правил с заданным именем
+*/
+DROP PROCEDURE IF EXISTS create_rules(TEXT, INT, INT);
+CREATE PROCEDURE create_rules( tableName TEXT, sensorNum INT, ruleNum INT)
+AS $$
+DECLARE
+	create_query TEXT := '';
+	counter INT := 0;
+BEGIN
+		create_query := format('DROP TABLE IF EXISTS %s;CREATE TABLE %s ( N serial PRIMARY KEY, rule_array REAL[]);', tableName, tableName);		
+		EXECUTE create_query;
+		CALL generate_rules_data(tableName, sensorNum, ruleNum);
+END ;
+$$
+LANGUAGE plpgsql ;
+
+/* Генерация правил
+tableName - имя таблицы
+sensorNum - количество сенсоров
+ruleNum - количество правил
+Результат выполения:
+В таблицу с именем tableName добавлены правила в количестве ruleNum
+*/
+DROP PROCEDURE IF EXISTS generate_rules_data(TEXT, INT, INT);
+CREATE PROCEDURE generate_rules_data( tableName TEXT, sensorNum INT, ruleNum INT)
+AS $$
+DECLARE
+	data_query TEXT := '';
+	counter INT := 0;
+	counterSensor INT := 0;
+	rule_array REAL[];
+BEGIN
+		WHILE counter < ruleNum
+		LOOP
+			counter := counter + 1;
+			counterSensor := 0;	
+			rule_array :='{}';
+			WHILE counterSensor < sensorNum 
+			LOOP
+				counterSensor := counterSensor + 1;
+				IF random() > 0.8 THEN
+					rule_array := array_append(rule_array, NULL);
+				ELSE
+					rule_array := array_append(rule_array, round((random() * 100))::real);
+				END IF;
+				
+			END LOOP;
+		    data_query := data_query || format('INSERT INTO %s (rule_array) values ( %L );', tableName, rule_array);
+	    END LOOP;
+		EXECUTE data_query;
+END ;
+$$
+LANGUAGE plpgsql ;
+
+/* Проверка линии агрегатов
+unitLine - входные данные для проверки линии агрегатов
+{
+		"lineThreshold": 0.8,
+		"units":[
+			{
+				"unitThreshold": 0.8,
+				"rulesTable": "r1",
+				"anomaly":[70, 7, 2, 75, 39, 21, 31, 34, 34, 13, 25, 64, 40] 
+			},
+			{
+				"unitThreshold": 0.8,
+				"rulesTable": "r2",
+				"anomaly":[62, 95, 59, 31, 60, 64, 46, 23, 2]
+			},
+			{
+				"unitThreshold": 0.8,
+				"rulesTable": "r3",
+				"anomaly":[56, 38, 44, 77, 29, 79, 99]
+			},
+			{
+				"unitThreshold": 0.8,
+				"rulesTable": "r4",
+				"anomaly":[71, 96, 7, 12, 67, 86, 35, 36, 91, 75, 19]
+			},
+			{
+				"unitThreshold": 0.8,
+				"rulesTable": "r5",
+				"anomaly":[33, 98, 80, 58, 37, 41, 81, 72, 43, 23, 83, 2, 29, 89, 49]
+			}
+		]
+	}
+	
+unitLine->lineThreshold - порог исправности линии	
+unitLine->units - массив с информацией об ашрегатах линии
+unitLine->units[]->unitThreshold - порог исправности агрегата
+unitLine->units[]->rulesTable - таблица правил для агрегата
+unitLine->units[]->anomaly - аномалия
+Возвращаемое значение:
+результат проверки линии
+{
+	"is_faulty" : true, 
+	"faulty_units" : [1,2,3,4,5], 
+	"units" : [
+		{
+			"is_faulty" : true, 
+			"anomaly_cause" : 2, 
+			"faulty_sensors" : [1,2,3,4,5,6,7,8,9,11,12,13]
+		},
+		{
+			"is_faulty" : true, 
+			"anomaly_cause" : 2, 
+			"faulty_sensors" : [2,3,4,5,6,7,8,9]
+		}
+	]
+}
+is_faulty - признак неисправности линии(true - неисправна, false - исправна)
+faulty_units - массив неисправных агрегатов
+units - массив с информацией по агрегатам
+units->is_faulty - признак неисправности агрегата(true - неисправен, false - исправен)
+units->anomaly_cause - код причины аномалии
+units->faulty_sensors - массив неисправных сенсоров
+*/
+DROP FUNCTION IF EXISTS testLine(JSON);
+CREATE FUNCTION testLine(unitLine JSON)
+RETURNS JSON
+AS $$
+DECLARE
+	res JSON;
+	res_units JSON[];
+	lineThreshold REAL;
+	faulty_units  INTEGER[];
+	unit_counter INTEGER := 0;
+	
+	unit_json JSON;
+	unit_threshold REAL;
+	unit_rules TEXT;
+	unit_anomaly TEXT;
+	unit_anomaly_array REAL[];
+	unit_anomaly_cause INTEGER;
+	unit_faulty_sensors INTEGER[];
+	unit_is_faulty BOOLEAN;
+	unit_res JSON;
+BEGIN
+	lineThreshold := unitLine->'lineThreshold';
+	RAISE NOTICE 'lineThreshold: %', lineThreshold;
+	FOR unit_json IN SELECT * FROM json_array_elements(unitLine->'units')
+	LOOP
+	    unit_counter := unit_counter + 1;
+	    unit_threshold := unit_json->'unitThreshold';
+		unit_rules :=  unit_json->>'rulesTable';
+		unit_anomaly := replace(replace(unit_json->>'anomaly', '[', '{'),']','}');
+		unit_anomaly_array := unit_anomaly::real[];
+
+		unit_anomaly_cause = anomaly_cause(unit_rules, unit_anomaly_array);
+		unit_faulty_sensors := faulty_sensors(unit_rules, unit_anomaly_array);
+		
+		unit_is_faulty := unit_threshold < array_length(unit_faulty_sensors, 1)::real/array_length(unit_anomaly_array, 1);
+		unit_res := json_build_object(
+			'is_faulty', unit_is_faulty,
+			'anomaly_cause', unit_anomaly_cause,
+			'faulty_sensors', unit_faulty_sensors
+		);
+		res_units := array_append(res_units, unit_res);
+		IF unit_is_faulty THEN
+			faulty_units = array_append(faulty_units, unit_counter);
+		END IF;
+	END LOOP;
+	res := json_build_object(
+		'is_faulty', lineThreshold < array_length(faulty_units, 1)::real/unit_counter,
+		'faulty_units', faulty_units,
+		'units', res_units
+	);
+RETURN 	res;
+END;
+$$
+LANGUAGE plpgsql;
 
--- 2.7_EXPLAINANOMALY/Test_EXPLAINANOMALY.sql
+++ 2.7_EXPLAINANOMALY/Test_EXPLAINANOMALY.sql
@@ -48,8 +48,48 @@ DECLARE
 		14,
 		18
 	];
+	json_input JSON := '{
+		"lineThreshold": 0.8,
+		"units":[
+			{
+				"unitThreshold": 0.8,
+				"rulesTable": "r1",
+				"anomaly":[70, 7, 2, 75, 39, 21, 31, 34, 34, 13, 25, 64, 40] 
+			},
+			{
+				"unitThreshold": 0.8,
+				"rulesTable": "r2",
+				"anomaly":[62, 95, 59, 31, 60, 64, 46, 23, 2]
+			},
+			{
+				"unitThreshold": 0.8,
+				"rulesTable": "r3",
+				"anomaly":[56, 38, 44, 77, 29, 79, 99]
+			},
+			{
+				"unitThreshold": 0.8,
+				"rulesTable": "r4",
+				"anomaly":[71, 96, 7, 12, 67, 86, 35, 36, 91, 75, 19]
+			},
+			{
+				"unitThreshold": 0.8,
+				"rulesTable": "r5",
+				"anomaly":[33, 98, 80, 58, 37, 41, 81, 72, 43, 23, 83, 2, 29, 89, 49]
+			}
+		]
+	}';
 BEGIN
+    
 	RAISE NOTICE 'Причина аномалии: %', anomaly_cause('rules', anomaly) ;
 	RAISE NOTICE 'Неисправные сенсоры: %', faulty_sensors('rules', anomaly) ;
+	
+	
+	CALL create_rules('r1', 13, 25);
+	CALL create_rules('r2', 9, 30);
+	CALL create_rules('r3', 7, 20);
+	CALL create_rules('r4', 11, 50);
+	CALL create_rules('r5', 15, 45);
+	RAISE NOTICE 'Проверка линии: %', testLine(json_input);
+	
 END ;
 $$
